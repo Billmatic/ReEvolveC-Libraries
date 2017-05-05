437439e58ec1941e047f9f474a3fba337d072192
Hitachi-wbojcevski <wbojcevski@hitachi-solutions.com>
2017-05-05 16:04:09 -0400

    Inclumn new update name javascript fuction

--- ReEvolveCSharpLibraries/LibraryTester/Program.cs
+++ ReEvolveCSharpLibraries/LibraryTester/Program.cs
@@ -18,14 +18,10 @@ namespace LibraryTester
         {
             int i = 0;
             CRMConnectionHelper crmConnectionHelper = new CRMConnectionHelper();
-
             IOrganizationService _service = crmConnectionHelper.GetCRMConnection("https://factor.api.crm.dynamics.com/XRMServices/2011/Organization.svc", "ideacateam@factor.ca", "ideaca4me!");
-
             QueryExpression selectAllApplicationComponents = new QueryExpression() { EntityName = "factor_applicationcomponent", ColumnSet = new ColumnSet(true) };
             EntityCollection collection = _service.RetrieveMultiple(selectAllApplicationComponents);
             EntityCollection collectionAll = _service.RetrieveMultipleAll(selectAllApplicationComponents);
-
-
         }
     }
 }
---
+++ ReEvolveCSharpLibraries/src/ReEvolveCRMLibrary/JavaScript/ReEvolveCRMForm.js
@@ -0,0 +1,44 @@
+﻿/**
+* concatenates and sets the text field from other fields on the form
+* @param {string} updatedfield the crm field name to be updated
+* @param {string} dependentFields: comma seperated string that replaces crm form attributes with 
+* the controls content.  Lookup, text and optionset are supported.
+* @param {int} nameLength:  This limits the name to the lenght give.  Should be set the 
+* target fields maximum length.
+* and text fields
+*/
+function UpdateTextFromFields(updatefield, dependentFields, nameLength)
+{
+    var name = '';
+    var dependentFieldsArry = dependentFields.split(',');
+
+    var arrayLength = dependentFieldsArry.length;
+    debugger;
+    for (var i = 0; i < arrayLength; i++) {
+        if (Xrm.Page.getAttribute(dependentFieldsArry[i]) != null) {
+            var dependentValue = Xrm.Page.getAttribute(dependentFieldsArry[i]).getValue();
+            if (dependentValue != null && typeof dependentValue === 'string') {
+                name += ' ' + dependentValue;
+            }
+            else if (dependentValue != null && typeof dependentValue === 'object') {
+                var dependentName = ' ' + dependentValue[0].name.trim();
+                name += dependentName;
+            }
+            else if (dependentValue != null && typeof dependentValue === 'number') {
+                var dependentName = ' ' + Xrm.Page.getAttribute(dependentFieldsArry[i]).getOption(dependentValue).text;
+                name += dependentName;
+            }
+        }
+        else
+        {
+            name += ' '+dependentFieldsArry[i];
+        }
+
+    }
+
+    if (name.length >= nameLength) {
+        name = name.substring(1, nameLength - 1).trim();
+    }
+
+    Xrm.Page.getAttribute(updatefield).setValue(name);
+};
