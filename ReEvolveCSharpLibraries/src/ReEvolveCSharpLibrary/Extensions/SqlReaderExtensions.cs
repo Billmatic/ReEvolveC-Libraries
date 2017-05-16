1922e25d37a4333f500da41bf4903541946a7dc2
Hitachi-wbojcevski <wbojcevski@hitachi-solutions.com>
2017-05-16 12:43:40 -0400

    Added new extensions and helpers

---
+++ ReEvolveCSharpLibraries/src/ReEvolveCSharpLibrary/Extensions/SqlReaderExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ReEvolveCSharpLibrary.Extensions
+{
+    public static class SqlReaderExtensions
+    {
+        private static string GetStringFromDB(this SqlDataReader reader, string columnName)
+        {
+            string data = "";
+            try
+            {
+                data = reader[columnName].ToString().Trim();
+            }
+            catch
+            {
+
+            }
+
+            return data;
+        }
+
+        private static bool? GetBoolFromDB(this SqlDataReader reader, string columnName)
+        {
+            bool? data = null;
+            try
+            {
+                data = (bool?)reader[columnName];
+            }
+            catch
+            {
+
+            }
+
+            return data;
+        }
+
+        private static DateTime GetDateTimeFromDB(this SqlDataReader reader, string columnName)
+        {
+            DateTime data = new DateTime();
+            try
+            {
+                data = Convert.ToDateTime(reader[columnName].ToString());
+            }
+            catch
+            {
+
+            }
+
+            return data;
+        }
+    }
+}
---
+++ ReEvolveCSharpLibraries/src/ReEvolveCSharpLibrary/Helpers/SQLDatabaseHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ReEvolveCSharpLibrary.Helpers
+{
+    public class SQLDatabaseHelper
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public SqlConnection ConnectToSQLDatabase(string serverName, string databaseName, string userName, string userPassword, string domain)
+        {
+            SqlConnection sqlConnection = new SqlConnection();
+            if (domain != null)
+            {
+                sqlConnection = new SqlConnection("user id=" + domain + "\\" + userName + ";" +
+                                       "password=" + userPassword + ";server=" + serverName + ";" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=" + databaseName + "; " +
+                                       "connection timeout=30");
+            }
+            else
+            {
+                sqlConnection = new SqlConnection(@"Server=tcp:"+serverName+";Database="+databaseName+";User ID="+userName+";Password="+userPassword+";MultipleActiveResultSets=True;Encrypt=true;TrustServerCertificate=false;Trusted_Connection=false;");
+            }
+            
+
+            return sqlConnection;
+        }
+
+    }
+
+    
+}
